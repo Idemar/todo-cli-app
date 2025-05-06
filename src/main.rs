9e051a60f6dac50278a45bec8972740b3f0282f4
Morten Idemar Andersen Tenmann <mortenidemar@gmail.com>
2025-05-06 21:41:23 +0200

    mere koding

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,53 @@
+use std::ops::Index;
+
 use ncurses::*;
 
 const REGULAR_PAIR: i16 = 0;
 const HIGHLIGHT_PAIR: i16 = 1;
 
+type Id = usize;
+
+#[derive(Default)]
+struct Ui {
+    list_curr: Option<Id>,
+}
+
+impl Ui {
+    fn start(&mut self) {
+        todo!();
+    }
+    
+    fn start_liste(&mut self, id: Id) {
+        todo!();
+    }
+
+    fn liste_elementer(&mut self, label: &str, id: Id) {
+        // let pair = {
+        //     if todo_curr == index {
+        //         HIGHLIGHT_PAIR
+        //     } else {
+        //         REGULAR_PAIR
+        //     }
+        // };
+        // attron(COLOR_PAIR(pair));
+        // mv(index as i32, 1);
+        // addstr(*todo);
+        // attroff(COLOR_PAIR(pair));
+        todo!();
+    }
+
+    fn label(&mut self, label: &str) {
+        todo!();
+    }
+
+    fn slutt_liste(&mut self) {
+        todo!();
+    }
+
+    fn slutt(&mut self) {
+        todo!();
+    }
+}
 fn main() {
     initscr();
     noecho();
@@ -15,28 +60,36 @@ fn main() {
     init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
 
     let mut quit = false;
-    let todos = vec![
-        "Lag todo app",
-        "Drikk en kopp kaffe",
-        "Fullfør det du begynte med",
+    let todos: Vec<String> = vec![
+        "Lag todo app".to_string(),
+        "Drikk en kopp kaffe".to_string(),
+        "Fullfør det du begynte med".to_string(),
     ];
 
-    let done = Vec::<String>::new();
-    let mut todo_curr: usize = 1;
+    let dones = Vec::<String>::new();
+    let mut done_curr: usize = 0;
+    let mut todo_curr: usize = 0;
+
+    let mut ui = Ui::default();
 
     while !quit {
-        for (index, todo) in todos.iter().enumerate() {
-            let pair = {
-                if todo_curr == index {
-                    HIGHLIGHT_PAIR
-                } else {
-                    REGULAR_PAIR
-                }
-            };
-            attron(COLOR_PAIR(pair));
-            mv(index as i32, 1);
-            addstr(*todo);
-            attroff(COLOR_PAIR(pair));
+        ui.start();
+        {
+            ui.start_liste(todo_curr);
+
+            for (index, todo) in todos.iter().enumerate() {
+                ui.liste_elementer(todo, index);
+            }
+            ui.slutt_liste();
+
+            ui.label("---------------------------------------------------");
+
+            ui.start_liste(done_curr);
+            for (Index, done) in dones.iter().enumerate() {
+                ui.liste_elementer(&done, Index);
+            }
+            ui.slutt_liste();
+            ui.slutt();
         }
 
         refresh();
