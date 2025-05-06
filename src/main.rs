724240b9c804765a37e6fd5eebfd221bb150a5c3
Morten Idemar Andersen Tenmann <mortenidemar@gmail.com>
2025-05-06 07:06:57 +0200

    q for å avslutte programmet
    Legginn 'q' tasten for å avslutte programmet.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ fn main() {
     initscr();
     addstr("Hei ncurses");
 
-    let quit = false;
+    let mut quit = false;
 
     while !quit {
         let key = getch();
+
+        match key as u8 as char {
+            'q' => quit = true,
+            _ => {}
+        }
     }
 
     endwin();
